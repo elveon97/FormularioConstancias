156075aeb091d88c1c989d3801c126f6a84fdc08
Darío Vázquez <theelveon97@gmail.com>
2019-01-23 11:53:14 -0600

    Agregado procedimientos para eliminar, leer y actualizar

--- BD/BD.sql
+++ BD/BD.sql
@@ -1,6 +1,7 @@
 -- --------------------------------------------------------------- 2019/01/21 --
 -- Desarrollado por Darío Alexis Vázquez Magaña
 -- theelveon97@gmail.com
+-- https://github.com/elveon97/FormularioConstancias
 -- -----------------------------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -158,5 +159,196 @@ BEGIN
 
   INSERT INTO constancia (evento, cursante, fecha_emision, comentario)
     VALUES (_evento_id, _cursante, _fecha_emision, _comentario);
+END //
+DELIMITER ;
+
+--  PROCEDIMIENTO BORRAR USUARIO
+DELIMITER //
+CREATE PROCEDURE borrar_usuario(
+  IN _usuario_id int(8)
+)
+BEGIN
+  DELETE FROM usuario WHERE usuario_id = _usuario_id;
+END //
+DELIMITER ;
+
+--  PROCEDIMIENTO BORRAR CURSANTE
+DELIMITER //
+CREATE PROCEDURE borrar_cursante(
+  IN _codigo varchar(20)
+)
+BEGIN
+  DELETE FROM cursante WHERE codigo = _codigo;
+END //
+DELIMITER ;
+
+--  PROCEDIMIENTO BORRAR TIPO DE EVENTO
+DELIMITER //
+CREATE PROCEDURE borrar_tipo_evento(
+  IN _tipo_evento_id int(8)
+)
+BEGIN
+  DELETE FROM tipo_evento WHERE tipo_evento_id = _tipo_evento_id;
+END //
+DELIMITER ;
+
+--  PROCEDIMIENTO BORRAR EVENTO
+DELIMITER //
+CREATE PROCEDURE borrar_evento(
+  IN _evento_id int(8)
+)
+BEGIN
+  DELETE FROM evento WHERE evento_id = _evento_id;
+END //
+DELIMITER ;
+
+--  PROCEDIMIENTO BORRAR CONSTANCIA
+DELIMITER //
+CREATE PROCEDURE borrar_constancias(
+  IN _folio int(8)
+)
+BEGIN
+  DELETE FROM constancia WHERE folio = _folio;
+END //
+DELIMITER ;
+
+--  PROCEDIMIENTO ACTUALIZAR USUARIO
+DELIMITER //
+CREATE PROCEDURE actualizar_usuario(
+  IN _usuario_id int(8),
+     _username varchar(40),
+     _password varchar(40),
+     _email varchar(254),
+     _tipo_usuario int(1)
+)
+BEGIN
+  UPDATE usuario SET username =  _username, password = _password,
+   email = _email, tipo_usuario = _tipo_usuario WHERE usuario_id = _usuario_id;
+END //
+DELIMITER ;
+
+--  PROCEDIMIENTO ACTUALIZAR CURSANTE
+DELIMITER //
+CREATE PROCEDURE actualizar_cursante(
+  IN _codigo varchar(20),
+     _nombre varchar(40)
+)
+BEGIN
+  UPDATE cursante SET nombre =  _nombre WHERE codigo = _codigo;
+END //
+DELIMITER ;
+
+--  PROCEDIMIENTO ACTUALIZAR TIPO DE EVENTO
+DELIMITER //
+CREATE PROCEDURE actualizar_tipo_evento(
+  IN _tipo_evento_id int(8),
+     _nombre varchar(40)
+)
+BEGIN
+  UPDATE tipo_evento SET nombre =  _nombre WHERE tipo_evento_id = _tipo_evento_id;
+END //
+DELIMITER ;
+
+--  PROCEDIMIENTO ACTUALIZAR EVENTO
+DELIMITER //
+CREATE PROCEDURE actualizar_evento(
+  IN _evento_id int(8),
+     _tipo_evento int(8),
+     _nombre varchar(40),
+     _duracion int(5),
+     _fecha_inicio date,
+     _fecha_fin date,
+     _instancia varchar(40)
+)
+BEGIN
+  UPDATE evento SET tipo_evento = _tipo_evento_id, nombre = _nombre,
+    duracion = _duracion, fecha_inicio = _fecha_inicio, fecha_fin = _fecha_fin,
+    instancia = _instancia WHERE evento_id = _evento_id;
+END //
+DELIMITER ;
+
+--  PROCEDIMIENTO ACTUALIZAR CONSTANCIA
+DELIMITER //
+CREATE PROCEDURE actualizar_constancia(
+  IN _folio int(8),
+     _evento int(8),
+     _cursante varchar(20),
+     _fecha_emision date,
+     _comentario varchar(255)
+)
+BEGIN
+  UPDATE evento SET evento = _evento, cursante = _cursante,
+    fecha_emision = _fecha_emision, comentario = _comentario WHERE folio = _folio;
+END //
+DELIMITER ;
+
+--  PROCEDIMIENTO LEER USUARIO
+DELIMITER //
+CREATE PROCEDURE leer_usuario(
+  IN _usuario_id int(8),
+  OUT _username varchar(40),
+      _password varchar(40),
+      _email varchar(254),
+      _tipo_usuario int(1)
+)
+BEGIN
+  SELECT username, password, email, tipo_usuario INTO _username, _password, _email,
+    _tipo_usuario FROM usuario WHERE usuario_id = _usuario_id;
+END //
+DELIMITER ;
+
+--  PROCEDIMIENTO LEER CURSANTE
+DELIMITER //
+CREATE PROCEDURE leer_cursante(
+  IN _codigo varchar(20),
+  OUT _nombre varchar(60)
+)
+BEGIN
+  SELECT nombre INTO _nombre FROM cursante WHERE codigo = _codigo;
+END //
+DELIMITER ;
+
+
+--  PROCEDIMIENTO LEER TIPO DE EVENTO
+DELIMITER //
+CREATE PROCEDURE leer_tipo_evento(
+  IN _tipo_evento_id int(8),
+  OUT _nombre varchar(60)
+)
+BEGIN
+  SELECT nombre INTO _nombre FROM tipo_evento WHERE tipo_evento_id = _tipo_evento_id;
+END //
+DELIMITER ;
+
+--  PROCEDIMIENTO LEER EVENTO
+DELIMITER //
+CREATE PROCEDURE leer_evento(
+  IN _evento_id int(8),
+  OUT _tipo_evento int(8),
+      _nombre varchar(40),
+      _duracion int(5),
+      _fecha_inicio date,
+      _fecha_fin date,
+      _instancia varchar(40)
+)
+BEGIN
+  SELECT tipo_evento, nombre, duracion, fecha_inicio, fecha_fin, instancia
+    INTO _tipo_evento, _nombre, _duracion, _fecha_inicio, _fecha_fin, _instancia
+    FROM evento WHERE evento_id = _evento_id;
+END //
+DELIMITER ;
+
+--  PROCEDIMIENTO LEER CONSTANCIA
+DELIMITER //
+CREATE PROCEDURE leer_constancia(
+  IN _folio int(8),
+  OUT _evento int(8),
+      _cursante varchar(20),
+      _fecha_emision date,
+      _comentario varchar(255)
+)
+BEGIN
+  SELECT evento, cursante, fecha_emision, comentario INTO _evento, _cursante,
+    _fecha_emision, _comentario FROM evento WHERE folio = _folio;
 END //
 DELIMITER ;
