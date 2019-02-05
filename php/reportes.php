079584882f400f779f218dff33d4c68460105ae4
Darío Vázquez <theelveon97@gmail.com>
2019-02-04 19:55:37 -0600

    Actualización funciones reportes 2

--- php/reportes.php
+++ php/reportes.php
@@ -19,4 +19,13 @@
     return mysqli_query($conexion,$sql);
   }
 
+  public function reporteCurso($nombreCurso) {
+    $obj = new conectar();
+    $conexion = $obj -> conexion();
+
+    $sql = "SELECT constancia.folio, evento.nombre, tipo_evento.nombre, cursante.nombre, instancia.nombre, fecha_inicio, fecha_fin FROM ((((constancia INNER JOIN evento ON constancia.evento = evento.evento_id) INNER JOIN instancia ON evento.instancia = instancia.instancia_id) INNER JOIN tipo_evento ON evento.tipo_evento = tipo_evento.tipo_evento_id) INNER JOIN cursante ON constancia.cursante = cursante.codigo) WHERE UPPER(TRIM(evento.nombre)) = UPPER(TRIM('".$nombreCurso."'))";
+
+    return mysqli_query($conexion,$sql);
+  }
+
 ?>
