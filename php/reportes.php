94fd29b5ba7a7002fd037f37e1006c06fec44003
Darío Vázquez <theelveon97@gmail.com>
2019-02-04 19:17:51 -0600

    Actualización funciones reportes

--- php/reportes.php
+++ php/reportes.php
@@ -5,7 +5,7 @@
     $obj = new conectar();
     $conexion = $obj -> conexion();
 
-    $sql = "SELECT constancia.folio, evento.nombre, tipo_evento.nombre, cursante.nombre, instancia.nombre, fecha_inicio, fecha_fin FROM ((((constancia INNER JOIN evento ON constancia.evento = evento.evento_id) INNER JOIN instancia ON evento.instancia = instancia.instancia_id) INNER JOIN tipo_evento ON evento.tipo_evento = tipo_evento.tipo_evento_id) INNER JOIN cursante ON constancia.cursante = cursante.codigo)";
+    $sql = "SELECT constancia.folio, evento.nombre, tipo_evento.nombre, cursante.nombre, instancia.nombre, fecha_inicio, fecha_fin FROM ((((constancia INNER JOIN evento ON constancia.evento = evento.evento_id) INNER JOIN instancia ON evento.instancia = instancia.instancia_id) INNER JOIN tipo_evento ON evento.tipo_evento = tipo_evento.tipo_evento_id) INNER JOIN cursante ON constancia.cursante = cursante.codigo) WHERE constancia.fecha_emision BETWEEN '$fechainicial' AND '$fechafinal'";
 
     return mysqli_query($conexion,$sql);
   }
