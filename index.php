cc57a867720396d22e73f214c2464159d76dcaff
Laptop Stalin <mags961224hjccmt00@gmail.com>
2019-02-15 00:28:01 +0100

    Pagina de Bienvenida

--- index.php
+++ index.php
@@ -1,138 +1,204 @@
+<!--
+Buscar iconos para la version 4.0 de fontaweso [Version que se maneja en este sistema]
+https://fontawesome.com/v4.7.0/icons/
+-->
+
+<?php
+$result = "";
+
+if($_POST){
+  require_once "php/conexion.php";
+
+  //Se reciben los parametros por _$GET
+  $folio = $_POST['folio'];
+
+  $obj = new conectar();
+  $conexion = $obj -> conexion();
+
+  $result = mysqli_query($conexion, "SELECT constancia.folio, evento.nombre, cursante.nombre, fecha_emision, comentario FROM ((constancia INNER JOIN evento ON constancia.evento = evento.evento_id) INNER JOIN cursante ON constancia.cursante = cursante.codigo) WHERE folio = $folio");
+
+}
+
+?>
 <!DOCTYPE html>
 
 <html>
 
-    <head>
-        <meta name="author" content="PracticantesServicioSocial">
-        <meta charset="utf-8">
+<head>
+  <meta name="author" content="PracticantesServicioSocial">
+  <meta charset="utf-8">
 
-        <link rel="stylesheet" href="styles/common_styles.css">
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+  <link rel="stylesheet" href="librerias/bootstrap/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="librerias/fontawesome/css/font-awesome.css">
+  <link rel="stylesheet" type="text/css" href="librerias/alertify/css/alertify.css">
+  <link rel="stylesheet" type="text/css" href="librerias/alertify/css/themes/bootstrap.css">
 
-        <link href="https://fonts.googleapis.com/css?family=Staatliches" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.5.2/css/buttons.dataTables.min.css">
 
-        <title>Iniciar sesión</title>
+  <script src="librerias/jquery.min.js"></script>
+  <script src="librerias/bootstrap/popper.min.js"></script>
+  <script src="librerias/bootstrap/bootstrap.min.js"></script>
+  <script src="librerias/alertify/alertify.js"></script>
 
-
-
-    </head>
-
-    <body>
-
-        <div class="m-center">
-            <div class="logo-container w-100">
-                <img src="img/logo.jpg" alt="CUSur">
-            </div>
-            <div class="colorFondo">
-                <!-- form Login -->
-                <form id ="loginForm" method="POST" action="">
-                    <!-- Usuario -->
-                    <label class="labelBlock" for="usuario">Usuario</label>
-                    <input class="inputBlockDesign" id="usuario" type="text" placeholder="Ingresa tu código de usuario" name="usser" style="margin-bottom: 30px;">
-                    <!-- Contraseña -->
-                    <label class="labelBlock" for="contraseña">Contraseña</label>
-                    <input class="inputBlockDesign" id="contraseña" type="password" placeholder="Ingresa tu contraseña" name="password">
-                    <!-- Submit Button -->
-                    <button class ="botonesFormulario" type="submit">Entrar</button>
-                </form>
-
-                <span id = "msg"> Por favor introduzca los datos solicitados </span>
-
-                <!--    Este script modifica el texto del span con id = "msg".
-                        Por lo tanto debe de ir despues de que el span haya sido declarado y antes de la condición donde será invocado! -->
-                <script type = "text/javascript">
-
-                    function mensajesLogueo(opcion){
-                        if(opcion == 1){
-                            document.getElementById("msg").innerHTML = "La contraseña introducida es incorrecta <br>";
-                        }else if(opcion == 2){
-                            document.getElementById("msg").innerHTML = "El usuario introducido no se encontró en la base de datos <br>";
-                        }
-                    }
-
-                </script>
+  <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 
 
-                <!-- Codigo php para validar el login al sistema -->
-                <?php
-                    //Código para realizar las consultas a la BD
-                    require("php/conexion.php");
-                    $obj = new conectar();
-                    $conn = $obj -> conexion();
+  <script type="text/javascript">
+  $(document).ready(function(){
+    //Cuando se clicke el boton para buscar la constancia...
+    $("#btnBuscarConstancia").click(function(){
+      //Obtener el folio de la constancia
+      var folioJS = $("#folioConstancia").val();
+      if(folioJS == '') {
+        alertify.error("Ingresa el folio de la constancia");
+        $("#resultadoBusquedaConstancia").css("display", "none");
+      }
+      else{
+        //Si el campo de la constancia no esta vacio hacer la busqueda de la constancia
+        //Enviar al archivo consultarConstancia.php el folio para validar que existe la constancia...
+        $.post("php/consultarConstancia.php", {"folio":folioJS}, function(data){
+          //En el valor data se encuentra el resultado del json despues de haber sido procesado por el archivo consultarConstancia.
+          if(data == 0){
+            alertify.error("El folio de la constancia ingresado no se encuentra en la base de datos");
+            $("#folioConstancia").val("");
+            $("#resultadoBusquedaConstancia").css("display", "none");
+          }
+          else{
+            $.post("php/getDatosConstancias.php", {"folio":data}, function(data){
+              //Cargarle los datos a la tabla!
+              $("#cuerpoTabla").html("");
+              var tr = `<tr>
+                          <td>`+data.folio+`</td>
+                          <td>`+data.nombreEvento+`</td>
+                          <td>`+data.nombreCursante+`</td>
+                          <td>`+data.fechaEmision+`</td>
+                          <td>`+data.comentarios+`</td>
+                        </tr>`;
+              $("#cuerpoTabla").append(tr)
+            },"json");
+            //Mostrar el container de la tabla si el folio ingresado si se encuentra en la base de datos...
+            $("#resultadoBusquedaConstancia").css("display", "block");
+          }
+        },"json");
+      }//Cierre else
+    });
 
-                    if ($_POST) {
-                        //Recibir las variables enviadas por el metodo POST
-                        $usser = validate_input($_POST["usser"]);
-                        $password = validate_input($_POST["password"]);
+    $('#tabla').DataTable({
+      "language": {
+        "sProcessing":     "Procesando...",
+        "sLengthMenu":     "Mostrar _MENU_ registros",
+        "sZeroRecords":    "No se encontraron resultados",
+        "sEmptyTable":     "Ningún dato disponible en esta tabla",
+        "sInfo":           "",
+        "sInfoEmpty":      "",
+        "sInfoFiltered":   "",
+        "sInfoPostFix":    "",
+        "sSearch":         "Buscar:",
+        "sUrl":            "",
+        "sInfoThousands":  ",",
+        "sLoadingRecords": "Cargando...",
+        "oPaginate": {
+          "sFirst":    "",
+          "sLast":     "",
+          "sNext":     "",
+          "sPrevious": ""
+        },
+        "oAria": {
+          "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+          "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+        }
+      },
+      "searching": false,
+      "bPaginate": false,
+    });
 
+  });
+  </script>
 
-                        //Validar que los datos proporcionados coincidan con un registro en la base de datos
-                        $result = $conn->query("SELECT username, password FROM usuario WHERE username = '".$usser."'");
+  <title>Página de Inicio</title>
 
-                        $resultadoConsultaUsuario = "";
-                        $resultadoConsultaPassword = "";
+</head>
 
-                        //Guardar en la variable $row el resultado de la consulta sql ---> Se almacena como tipo array, cada columna obtenida es una posicion del array!
-                        $row=mysqli_fetch_array($result);
-                        $resultadoConsultaUsuario = $row[0];
-                        $resultadoConsultaPassword = $row[1];
+<body>
 
-                        if($resultadoConsultaUsuario == $usser && $resultadoConsultaPassword == $password){
-                            //Realizar la consulta para obtener el tipo de usuario del usuario introducido
-                            $result = $conn->query("SELECT tipo_usuario FROM usuario where username = '".$usser."'");
-                            $tipoUsuario = "";
+  <div class="container">
+    <div class="row align-items-center justify-content-between">
+      <div class="col-4">
+        <img src="img/logo.jpg" alt="CUSur">
+      </div>
+      <div class="col-2">
+        <!-- Substituir la referencia de index.php por el nombre final que tendra el archivo del login -->
+        <a href="loguin.php" class="btn btn-info">
+          <span class="fa fa-sign-in mr-1"></span>
+          Iniciar Sesión
+        </a>
+      </div>
+    </div>
+  </div>
 
-                            while($row=mysqli_fetch_array($result)) {
-                                //Asignar el tipo de usuario a la variable correspondiente para realizar el direccionamiento
-                                $tipoUsuario = $row[0];
-                            }
+  <div class="container">
 
-                            mysqli_free_result($result);
+    <div class="row justify-content-center">
+      <div class="col-4">
+        <h3><span class="badge">Libro digital de constancias CUSUR</span></h3>
+      </div>
+    </div>
 
-                            //En este punto, ya se valido que las credenciales de acceso proporcionadas existen en la base de datos, es aquí donde se debe de iniciar la variable de sesion!
-                            //Metodo para iniciar la sesión
-                            session_start();
-                            //Iniciar la variable de sesion con el usuario que se proporciono
-                            $_SESSION['usuario'] = $usser;
+    <div class="row justify-content-center">
+      <div class="w-75 col-8">
+        <span class="text-justify">En el presente sistema encontraras registrados el total de reconocimientos,diplomas y Constancias
+          emitidos en las diferentes dependencias del Centro Universitario del Sur, con el fin de consulta
+          y validación de las mismas.
+        </span>
+      </div>
+    </div>
 
+    <div class="row justify-content-center">
+      <div class="col-0" style="margin-top:4em;">
+        <h4><span>Buscador</span></h4>
+      </div>
+    </div>
 
-                            //Realizar el direccionamiento dependiendo el tipo de usuario
-                            if($tipoUsuario == 0){
-                                //Redireccionar al Panel del Administrador
-                                header("Location: adminIndex.php");
-                            }else if($tipoUsuario == 1){
-                                //Redireccionar al formulario
-                                header("Location: formulario.php");
-                            }
-                        }else if($resultadoConsultaUsuario == $usser && $resultadoConsultaPassword != $password){
-                            echo '<script> mensajesLogueo(1) </script>';
-                        }else{
-                            echo '<script> mensajesLogueo(2) </script>';
-                        }
+    <div class="form-group form-row justify-content-center">
+      <label class="col-form-label" style="margin-rigth:0px;"><span class="fa fa-asterisk text-success" style="font-size:0.7rem;"></span>Número de Folio</label>
+      <div class="col-4">
+        <input id="folioConstancia" type="text" class="form-control" placeholder="Ingresa el número de folio de la constancia" name="folioConstancia" required>
+      </div>
+      <div class="col-1">
+        <button type="submit" class="btn btn-primary" id="btnBuscarConstancia">Buscar constancia</button>
+      </div>
+    </div>
+  </div>
 
-                        mysqli_free_result($result);
-                        mysqli_close($conn);
-                    }
+  <div class="container" id="resultadoBusquedaConstancia" style="display: none;">
+    <div class="col-lg">
+      <div class="card">
+        <div class="card-header">
 
-                    function validate_input($data) {
-                        $data = trim($data);
-                        $data = stripslashes($data);
-                        $data = htmlspecialchars($data);
-                        return $data;
-                    }
+          <div class="card-body">
+            <table id="tabla" class="display nowrap">
+              <thead>
+                <tr>
+                  <th>Folio</th>
+                  <th>Nombre capacitación</th>
+                  <th>Nombre cursante</th>
+                  <th>Fecha emisión constancia</th>
+                  <th>Comentarios</th>
+                </tr>
+              </thead>
+              <tbody id="cuerpoTabla">
 
-                ?>
-
-            </div>
-
+              </tbody>
+            </table>
+          </div>
         </div>
 
+      </div>
+    </div>
+  </div>
 
 
-
-
-    </body>
-
-
+</body>
 
 </html>
